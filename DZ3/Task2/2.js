98a6857aa937282908c7e70d964bfc8b46841250
Konstantin <tumandurman96@gmail.com>
2023-02-03 12:40:57 +0300

    DZ3 Task1 && Task2

--- DZ3/Task2/2.js
+++ DZ3/Task2/2.js
@@ -1,7 +1,8 @@
 alert("Привет, в следующем диалоговом окне введи свое имя:");
-let voidname = prompt("Введи свое имя:")
-function greeting(voidname) {
-    console.log(`Привет от JS! ${voidname}.`);
-    return voidname;
+let name = prompt("Введи свое имя:");
+function greeting(name) {
+    console.log(`Привет от JS! ${name}.`);
+    return name;
 }
-greeting(voidname);
+greeting(name);
+
